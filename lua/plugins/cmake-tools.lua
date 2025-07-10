bfdca7b01a3253fdcf2a285698ad358cb6b8b12e
Theodore-Chen <qinchen.zhang@qq.com>
2025-07-10 21:51:17 +0800

    Update cmake-tools plugin config

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -10,7 +10,7 @@ return {
   },
 
   keys = {
-    { "<leader>cg", "<cmd>CMakeGenerate<cr>", desc = "CMake: Configure Project" },
+    { "<leader>cg", "<cmd>CMakeGenerate!<cr>", desc = "CMake: Generate native build system" },
     { "<leader>cb", "<cmd>CMakeBuild<cr>", desc = "CMake: Build Target" },
     { "<leader>cB", "<cmd>CMakeBuildCurrentFile<cr>", desc = "CMake: Build targets related to current file" },
     { "<leader>ct", "<cmd>CMakeSelectBuildTarget<cr>", desc = "CMake: Select Build Target" },
@@ -23,20 +23,17 @@ return {
     { "<leader>cE", "<cmd>CMakeEditCache<cr>", desc = "CMake: Edit Cache" },
   },
 
-  opts = {
-    cmake_tools = {
-      cmake_build_directory = "build",
-      cmake_regenerate_on_save = true,
-      cmake_build_tool = "auto",
-      cmake_build_before_run = true,
-      cmake_configure_args = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=1" },
-    },
-    telescope = {
-      preview_location = "bottom",
-    },
-  },
-
-  config = function(_, opts)
-    require("cmake-tools").setup(opts)
-  end,
+  -- config = function()
+  --   vim.notify("[CMake-Tools] My config function IS RUNNING!", vim.log.levels.WARN)
+  --   require("cmake-tools").setup({
+  --     cmake_build_directory = "build",
+  --     cmake_build_tool = "ninja",
+  --     cmake_regenerate_on_save = true,
+  --     cmake_build_before_run = true,
+  --     cmake_configure_args = { "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" },
+  --     telescope = {
+  --       preview_location = "bottom",
+  --     },
+  --   })
+  -- end,
 }
